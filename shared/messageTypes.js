5ccb1797a0c01c75bf799bb78dac51d10547dc41
Evil-Devil <evil_devil@gmx.net>
2016-02-11 20:45:45 +0100

    changed messagetypes to return an object

--- shared/messageTypes.js
+++ shared/messageTypes.js
@@ -1,7 +1,9 @@
-var MessageTypes = {
-    USER_CONNECTED: 'user connected',
-    CHAT_MESSAGE: 'chat message',
-    PLAYER_NAME_SET: 'player name set'
+var MessageTypes = function() {
+    return {
+        USER_CONNECTED: 'user connected',
+        CHAT_MESSAGE: 'chat message',
+        PLAYER_NAME_SET: 'player name set'
+    }
 };
 
 module.exports = MessageTypes;
