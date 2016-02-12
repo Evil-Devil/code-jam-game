33b66526180b94c103d3fdbc1b3de0b38378426a
Konstantin Koch <koch.konstantin@yahoo.de>
2016-02-12 10:18:08 +0100

    added overlay for username

--- client/game.js
+++ client/game.js
@@ -133,7 +133,6 @@ var player = new Player(-1, socket);
 player.onNameChanged = function (name) {
     socket.emit(MessageTypes.PLAYER_NAME_SET, name);
 };
-player.setName("TestUser" + Math.random().toString(36).substring(2, 5));
 
 var market = new Marketplace();
 
@@ -153,6 +152,23 @@ var lastFrameTimeMs = 0,
     maxFPS = 60,
     delta = 0,
     timestep = 1000 / 60;
+
+
+var nameForm = document.getElementById('nameForm');
+nameForm.onsubmit = function () {
+    nameForm.style.display = 'none';
+
+    var overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+
+    var chatForm = document.getElementById('chatBox');
+    chatForm.style.display = 'block';
+
+    var userNameInput = document.getElementById('userName');
+    player.setName(userNameInput.value);
+
+    return false;
+}
 
 // register objects for events
 engine.registerListener('click', transport.click);
